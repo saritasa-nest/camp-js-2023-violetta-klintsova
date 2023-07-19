aed3dded7b82a987cf42f662a756c73c159709ed
Violetta Klintsova <violetta.klintsova@saritasa.com>
2023-07-19 13:38:04 +0700

    Add methods to anime component JC-541

--- apps/angular/src/app/features/anime-table/anime-table/anime-table.component.ts
+++ apps/angular/src/app/features/anime-table/anime-table/anime-table.component.ts
@@ -2,23 +2,28 @@ import { Component, OnInit } from '@angular/core';
 import { AnimeService } from '@js-camp/angular/core/services/anime.service';
 import { AnimeItem } from '@js-camp/core/models/animeItem';
 
+/** Anime table component. */
 @Component({
 	selector: 'anime-table',
 	templateUrl: './anime-table.component.html',
 	styleUrls: ['./anime-table.component.css'],
 })
+export class AnimeTableComponent implements OnInit {
+	/** Array of anime items. */
+	public animeList: AnimeItem[] = [];
 
-export class AnimeTableComponent {
-	private readonly animeList: AnimeItem[] = [];
+	/** Columns to be displayed in the table. */
+	public readonly displayedColumns: string[] = ['titleEng', 'image', 'titleJpn', 'airedStartDate', 'type', 'status'];
 
 	public constructor(private animeService: AnimeService) {}
 
-	// ngOnInit(): void {
-	// 	this.getAnimeList();
-	// }
+	/** Gets list of anime when component on init. */
+	public ngOnInit(): void {
+		this.getAnimeList();
+	}
 
-	// getAnimeList(): void {
-	// 	this.heroService.getAnimeList().subscribe((animeList) => (this.animeList = animeList));
-	// }
-
+	/** Gets anime list. */
+	public getAnimeList(): void {
+		this.animeService.getAnimeList().subscribe((animeList: AnimeItem[]) => (this.animeList = animeList));
+	}
 }
