3b307ccfe324581d9990003070e1fcdc9d9f6e74
Violetta Klintsova <violetta.klintsova@saritasa.com>
2023-07-19 13:39:46 +0700

    Add correct types to request JC-541

--- apps/angular/src/core/services/anime.service.ts
+++ apps/angular/src/core/services/anime.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
-import { Observable, of } from 'rxjs';
-import { catchError, map, tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { AnimeItem } from '@js-camp/core/models/animeItem';
 import { AnimeItemDto } from '@js-camp/core/dtos/animeItem.dto';
@@ -12,26 +12,24 @@ import { AnimeItemMapper } from '@js-camp/core/mappers/animeItem.mapper';
 
 import { environment } from '@js-camp/angular/environments/environment';
 
+/** Service to get different type of anime data. */
 @Injectable({
 	providedIn: 'root',
 })
-
-/** Service to get different type of anime data. */
 export class AnimeService {
+	private animeUrl = environment.apiUrl;
 
-	private animeUrl = environment.animeUrl;
-
-	private httpOptions = {
-		headers: new HttpHeaders({ 'Api-Key': environment.apiKey }),
-	};
+	private headers = new HttpHeaders().set('Api-Key', environment.apiKey);
 
 	private constructor(private http: HttpClient) {}
 
-	/** Get all Anime items. */
-	public getAnimeItems(): Observable<AnimeItem[]> {
-		const path = '/anime/anime/';
+	/** Gets anime list. */
+	public getAnimeList(): Observable<AnimeItem[]> {
+		const path = 'anime/anime/';
 		const url = `${this.animeUrl}${path}`;
-		const animeDto: AnimeResponseDto<AnimeItemDto> = this.http.get<AnimeResponseDto<AnimeItemDto>>(url, this.httpOptions);
-		return animeDto.results.map(el => AnimeItemMapper.fromDto(el));
+		return this.http.get<AnimeResponseDto<AnimeItemDto>>(url, { headers: this.headers }).pipe(
+			map((el: AnimeResponseDto<AnimeItemDto>) => el.results),
+			map((items: AnimeItemDto[]) => items.map((i: AnimeItemDto) => AnimeItemMapper.fromDto(i))),
+		);
 	}
 }
