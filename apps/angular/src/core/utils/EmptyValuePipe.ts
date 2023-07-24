f444a6ff302f70f1a2a036d316f2f1fc3351b29a
Violetta Klintsova <violetta.klintsova@saritasa.com>
2023-07-24 13:09:07 +0700

    Create custom pipe for empty values JC-541

---
+++ apps/angular/src/core/utils/EmptyValuePipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+/** Pipe to display 'Unknown' in case of an empty string. */
+@Pipe({
+	name: 'emptyValue',
+})
+export class EmptyValue implements PipeTransform {
+	/**
+	 * @param value Original string.
+	 * @returns Original string or 'unknown.
+	 */
+	public transform(value: string): string {
+		return value || 'Unknown';
+	}
+}
