13de9f2c4e95b6c79bba063d9e9ca8dea61b08d7
Violetta Klintsova <violetta.klintsova@saritasa.com>
2023-07-24 09:06:16 +0700

    Refactor JSDoc, rename interceptor JC-541

--- apps/angular/src/core/interceptors/animeService.interceptor.ts
+++ apps/angular/src/core/interceptors/animeService.interceptor.ts
@@ -6,13 +6,9 @@ import { environment } from '@js-camp/angular/environments/environment';
 
 /** Interceptor to handle authorization.  */
 @Injectable()
-export class AnimeServiceInterceptor implements HttpInterceptor {
+export class ApiKeyInterceptor implements HttpInterceptor {
 
-	/**
-	 * Intercepts the request to the server and adds authorization info.
-	 *  @param request Outgoing request.
-	 *  @param next Sends cloned request with header to the next handler.
-	 */
+	/** @inheritdoc */
 	public intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
 		const authRequest = request.clone({
 			headers: request.headers.set('Api-Key', environment.apiKey),
