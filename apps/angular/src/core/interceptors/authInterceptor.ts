e2db5818ab3d84071317f504f833c9beb087b244
Violetta Klintsova <violetta.klintsova@saritasa.com>
2023-07-31 17:23:03 +0700

    Add auth interceptor JC-553

---
+++ apps/angular/src/core/interceptors/authInterceptor.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { StorageService } from '../services/auth-storage.service';
+
+/** Interceptor to handle auth tokens. */
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+	public constructor(private readonly storage: StorageService) {}
+
+	/** @inheritdoc */
+	public intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+		const token = this.storage.getUser();
+
+		if (!token) {
+			return next.handle(request);
+		}
+
+		const requestWithToken = request.clone({
+			headers: request.headers.set('Authorization', `Bearer ${token}`),
+		});
+
+		return next.handle(requestWithToken);
+	}
+}
