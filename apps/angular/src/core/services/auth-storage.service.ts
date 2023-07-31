a40a99287e38eb3810e3dd2ebe100c868c0add41
Violetta Klintsova <violetta.klintsova@saritasa.com>
2023-07-31 17:23:38 +0700

    Refactor Storage service JC-553

--- apps/angular/src/core/services/auth-storage.service.ts
+++ apps/angular/src/core/services/auth-storage.service.ts
@@ -11,22 +11,22 @@ export class StorageService {
 	 * @param refresh Refresh token.
 	 */
 	public setUser(refresh: string): void {
-		window.localStorage.setItem('user', refresh);
+		window.localStorage.setItem('user-token', refresh);
 	}
 
 	/** Get current user. */
-	public getUser(): void {
-		window.localStorage.getItem('user');
+	public getUser(): string | null {
+		return window.localStorage.getItem('user-token');
 	}
 
 	/** Remove current user. */
 	public removeUser(): void {
-		window.localStorage.removeItem('user');
+		window.localStorage.removeItem('user-token');
 	}
 
 	/** Checks if the user is logged in. */
 	public isLoggedIn(): boolean {
-		const user = window.localStorage.getItem('user');
+		const user = window.localStorage.getItem('user-token');
 		if (user) {
 			return true;
 		}
