671c356eecf003fdcaae6ec8b92802b6402fc8a8
Violetta Klintsova <violetta.klintsova@saritasa.com>
2023-08-02 11:17:14 +0700

    Refactor auth storage service to handle both tokens changes JC-553

--- apps/angular/src/core/services/auth-storage.service.ts
+++ apps/angular/src/core/services/auth-storage.service.ts
@@ -10,23 +10,37 @@ export class StorageService {
 	 * Save current user.
 	 * @param refresh Refresh token.
 	 */
-	public setUser(refresh: string): void {
-		window.localStorage.setItem('user-token', refresh);
+	public setRefreshToken(refresh: string): void {
+		window.localStorage.setItem('refresh', refresh);
 	}
 
-	/** Get current user. */
-	public getUser(): string | null {
-		return window.localStorage.getItem('user-token');
+	/** @returns Current user refresh token. */
+	public getRefreshToken(): string | null {
+		return window.localStorage.getItem('refresh');
+	}
+
+	/**
+	 * Saves access token.
+	 * @param access Access token.
+	 */
+	public setAccessToken(access: string): void {
+		window.localStorage.setItem('access', access);
+	}
+
+	/** @returns Current user access token. */
+	public getAccessToken(): string | null {
+		return window.localStorage.getItem('access');
 	}
 
 	/** Remove current user. */
-	public removeUser(): void {
-		window.localStorage.removeItem('user-token');
+	public removeTokens(): void {
+		window.localStorage.removeItem('access');
+		window.localStorage.removeItem('refresh');
 	}
 
 	/** Checks if the user is logged in. */
 	public isLoggedIn(): boolean {
-		const user = window.localStorage.getItem('user-token');
+		const user = window.localStorage.getItem('access');
 		if (user) {
 			return true;
 		}
