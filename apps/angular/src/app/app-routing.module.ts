1d5ceb405a24fd2fad77c826434cb2fe087af1a1
Violetta Klintsova <violetta.klintsova@saritasa.com>
2023-08-02 14:50:24 +0700

    Refactor main app routes JC-553

--- apps/angular/src/app/app-routing.module.ts
+++ apps/angular/src/app/app-routing.module.ts
@@ -6,17 +6,26 @@ import { AnimeTableComponent } from './features/anime-table/anime-table/anime-ta
 import { LoginComponent } from './features/auth/log-in/log-in.component';
 import { SignUpComponent } from './features/auth/sign-up/sign-up.component';
 import { LandingComponent } from './landing/landing.component';
+import { MainLayoutComponent } from './main-layout/main-layout.component';
+import { EmptyLayoutComponent } from './empty-layout/empty-layout.component';
 
 const routes: Routes = [
 	{ path: '', component: LandingComponent },
-	{ path: 'anime', component: AnimeTableComponent },
-	{ path: 'log-in', component: LoginComponent },
-	{ path: 'sign-up', component: SignUpComponent },
+	{ path: 'anime', component: MainLayoutComponent, children: [{ path: '', component: AnimeTableComponent }] },
+	{
+		path: 'auth',
+		component: EmptyLayoutComponent,
+		children: [
+			{ path: '', redirectTo: 'log-in', pathMatch: 'full' },
+			{ path: 'log-in', component: LoginComponent },
+			{ path: 'sign-up', component: SignUpComponent },
+		],
+	},
 ];
 
 /** App routing module. */
 @NgModule({
-	imports: [RouterModule.forRoot(routes)],
+	imports: [RouterModule.forRoot(routes, { enableTracing: false })],
 	exports: [RouterModule],
 	providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
 })
