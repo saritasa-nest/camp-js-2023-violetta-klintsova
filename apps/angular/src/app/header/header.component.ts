8559acc0a3d2344b9ec0b380b3f946187e573f3f
Violetta Klintsova <violetta.klintsova@saritasa.com>
2023-08-02 14:52:16 +0700

    Add log out button JC-553

--- apps/angular/src/app/header/header.component.ts
+++ apps/angular/src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { StorageService } from '@js-camp/angular/core/services/auth-storage.service';
 
 /** Header component. */
 @Component({
@@ -6,4 +8,16 @@ import { Component } from '@angular/core';
 	templateUrl: './header.component.html',
 	styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+
+	/** User authorization status. */
+	protected isLoggedIn = this.storage.isLoggedIn();
+
+	public constructor(private readonly storage: StorageService, private readonly router: Router) {}
+
+	/** Logs the user out. */
+	protected onLogOut(): void {
+		this.storage.logOut();
+		this.router.navigate(['']);
+	}
+}
