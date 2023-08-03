28d03a7242b36b06ee345adb8fae95d64749cda7
Violetta Klintsova <violetta.klintsova@saritasa.com>
2023-08-03 16:24:24 +0700

    Refactor auth service to verify token if it exists on app initialization JC-553

--- apps/angular/src/core/services/auth.service.ts
+++ apps/angular/src/core/services/auth.service.ts
@@ -4,10 +4,13 @@ import { RegistrationInfo } from '@js-camp/core/models/registration-info';
 import { environment } from '@js-camp/angular/environments/environment';
 import { AuthDto } from '@js-camp/core/dtos/auth.dto';
 import { AuthMapper } from '@js-camp/core/mappers/auth.mapper';
-import { Observable, map } from 'rxjs';
+import { BehaviorSubject, Observable, map } from 'rxjs';
 import { RegistrationInfoMapper } from '@js-camp/core/mappers/registration-info.mapper';
 import { LoginInfo } from '@js-camp/core/models/login-info';
 import { Auth } from '@js-camp/core/models/auth';
+import { Router } from '@angular/router';
+
+import { StorageService } from './auth-storage.service';
 
 /** Authentification service. */
 @Injectable({
@@ -16,7 +19,41 @@ import { Auth } from '@js-camp/core/models/auth';
 export class AuthService {
 	private readonly apiUrl = environment.apiUrl;
 
-	public constructor(private readonly http: HttpClient) {}
+	/** User log in state. */
+	private userStateSubject$ = new BehaviorSubject<boolean>(false);
+
+	/** Returns the state subject as an observable. */
+	public get userState$(): Observable<boolean> {
+		return this.userStateSubject$.asObservable();
+	}
+
+	/**
+	 * Updates user state subject with supplied value.
+	 * @param value Value.
+	 */
+	public updateUserState(value: boolean): void {
+		this.userStateSubject$.next(value);
+	}
+
+	public constructor(
+		private readonly http: HttpClient,
+		private readonly router: Router,
+		private readonly storage: StorageService,
+	) {
+		const accessToken = this.storage.getAccessToken();
+
+		if (accessToken !== null) {
+			this.verifyToken(accessToken).subscribe({
+				next: () => {
+					this.userStateSubject$.next(true);
+				},
+				error() {
+					// TODO Redirect in case of an error
+					throw new Error('Verification error');
+				},
+			});
+		}
+	}
 
 	/**
 	 * User login.
@@ -25,9 +62,7 @@ export class AuthService {
 	public login(loginInfo: LoginInfo): Observable<Auth> {
 		const path = 'auth/login/';
 		const url = new URL(path, this.apiUrl);
-		return this.http
-			.post<AuthDto>(url.toString(), loginInfo)
-			.pipe(map(el => AuthMapper.fromDto(el)));
+		return this.http.post<AuthDto>(url.toString(), loginInfo).pipe(map(el => AuthMapper.fromDto(el)));
 	}
 
 	/**
@@ -38,9 +73,7 @@ export class AuthService {
 		const path = 'auth/register/';
 		const url = new URL(path, this.apiUrl);
 		const mappedRegisterData = RegistrationInfoMapper.toDto(registerInfo);
-		return this.http
-			.post<AuthDto>(url.toString(), mappedRegisterData)
-			.pipe(map(el => AuthMapper.fromDto(el)));
+		return this.http.post<AuthDto>(url.toString(), mappedRegisterData).pipe(map(el => AuthMapper.fromDto(el)));
 	}
 
 	/**
@@ -65,4 +98,10 @@ export class AuthService {
 		return this.http.post<string>(url.toString(), { token: access });
 	}
 
+	/** Logs a user out. */
+	public logOut(): void {
+		this.storage.deleteTokens();
+		this.router.navigate(['']);
+		this.userStateSubject$.next(false);
+	}
 }
