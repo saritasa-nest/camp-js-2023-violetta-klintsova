f985f33bdcb39f23cffbe83ff334cb1fb83c5e1a
Violetta Klintsova <violetta.klintsova@saritasa.com>
2023-08-07 16:21:29 +0700

    Add conditional router link to the landing page JC-553

--- apps/angular/src/app/header/header.component.ts
+++ apps/angular/src/app/header/header.component.ts
@@ -1,7 +1,5 @@
 import { Component, DestroyRef } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { Router } from '@angular/router';
-import { StorageService } from '@js-camp/angular/core/services/auth-storage.service';
 import { AuthService } from '@js-camp/angular/core/services/auth.service';
 
 /** Header component. */
@@ -14,15 +12,13 @@ export class HeaderComponent {
 	/** User authorization status. */
 	protected isLoggedIn!: boolean;
 
-	public constructor(
-		private readonly storage: StorageService,
-		private readonly router: Router,
-		private readonly auth: AuthService,
-		private readonly destroyRef: DestroyRef,
-	) {
-		auth.userState$().pipe(takeUntilDestroyed(this.destroyRef)).subscribe(value => {
-			this.isLoggedIn = value;
-		});
+	public constructor(private readonly auth: AuthService, private readonly destroyRef: DestroyRef) {
+		auth
+			.userState$()
+			.pipe(takeUntilDestroyed(this.destroyRef))
+			.subscribe(value => {
+				this.isLoggedIn = value;
+			});
 	}
 
 	/** Logs the user out. */
--- apps/angular/src/app/landing/landing.component.ts
+++ apps/angular/src/app/landing/landing.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { AuthService } from '@js-camp/angular/core/services/auth.service';
 
+/** Landing component. */
 @Component({
 	selector: 'camp-landing',
 	templateUrl: './landing.component.html',
 	styleUrls: ['./landing.component.css'],
 })
-export class LandingComponent {}
+export class LandingComponent {
+	/** User authorization status. */
+	protected isLoggedIn!: boolean;
+
+	public constructor(private readonly auth: AuthService, private readonly destroyRef: DestroyRef) {
+		auth
+			.userState$()
+			.pipe(takeUntilDestroyed(this.destroyRef))
+			.subscribe(value => {
+				this.isLoggedIn = value;
+			});
+	}
+}
