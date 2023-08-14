9df1c26e53c054de94cb505d7f9e8f1b9c65dc04
Violetta Klintsova <violetta.klintsova@saritasa.com>
2023-08-14 08:35:42 +0700

    Add changeDetector to the landing component JC-559

--- apps/angular/src/app/landing/landing.component.ts
+++ apps/angular/src/app/landing/landing.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, DestroyRef } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, DestroyRef } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { AuthService } from '@js-camp/angular/core/services/auth.service';
 
@@ -13,11 +13,16 @@ export class LandingComponent {
 	/** User authorization status. */
 	protected isLoggedIn!: boolean;
 
-	public constructor(private readonly auth: AuthService, private readonly destroyRef: DestroyRef) {
+	public constructor(
+		private readonly auth: AuthService,
+		private readonly destroyRef: DestroyRef,
+		private readonly changeDetector: ChangeDetectorRef,
+	) {
 		auth
 			.userState$()
 			.pipe(takeUntilDestroyed(this.destroyRef))
 			.subscribe(value => {
+				this.changeDetector.markForCheck();
 				this.isLoggedIn = value;
 			});
 	}
