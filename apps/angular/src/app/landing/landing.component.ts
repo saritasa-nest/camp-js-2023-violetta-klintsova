566f1585138ea0e7e1606e049ccddac6a7a9f01c
Violetta Klintsova <violetta.klintsova@saritasa.com>
2023-08-15 08:22:33 +0700

    Remove subscription from the landing component JC-553

--- apps/angular/src/app/landing/landing.component.ts
+++ apps/angular/src/app/landing/landing.component.ts
@@ -1,6 +1,4 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, DestroyRef } from '@angular/core';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { AuthService } from '@js-camp/angular/core/services/auth.service';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 /** Landing component. */
 @Component({
@@ -9,21 +7,4 @@ import { AuthService } from '@js-camp/angular/core/services/auth.service';
 	styleUrls: ['./landing.component.css'],
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LandingComponent {
-	/** User authorization status. */
-	protected isLoggedIn!: boolean;
-
-	public constructor(
-		private readonly auth: AuthService,
-		private readonly destroyRef: DestroyRef,
-		private readonly changeDetector: ChangeDetectorRef,
-	) {
-		auth
-			.userState$()
-			.pipe(takeUntilDestroyed(this.destroyRef))
-			.subscribe(value => {
-				this.changeDetector.markForCheck();
-				this.isLoggedIn = value;
-			});
-	}
-}
+export class LandingComponent {}
