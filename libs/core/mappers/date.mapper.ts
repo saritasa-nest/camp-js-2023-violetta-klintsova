d09c1ae8b3a76ca9a5092771a0b8fd63b53df3bf
Violetta Klintsova <violetta.klintsova@saritasa.com>
2023-08-29 11:58:11 +0700

    Add a method to convert Date to string JC-569

--- libs/core/mappers/date.mapper.ts
+++ libs/core/mappers/date.mapper.ts
@@ -8,4 +8,12 @@ export namespace DateMapper {
 	export function fromDto(date: string | null): Date | null {
 		return date ? new Date(date) : null;
 	}
+
+	/**
+	 * Maps a Date object to a string.
+	 * @param date Date an an object.
+	 */
+	export function toDto(date: Date | null): string | null {
+		return date ? date.toISOString() : null;
+	}
 }
